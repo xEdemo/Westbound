81a8ad85807741b470a1869ecf6ef0f77f30563a
Xnrrrrrr <Xnrrrrrr@gmail.com>
2024-12-20 16:00:30 -0500

    modified Item.js Renamed crime.mongodb.js

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -18,7 +18,12 @@ const ItemSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
-		// Will be dynamic
+		tier: {
+			type: String,
+			required: false,
+		},
+
+		// future dynamic handling of economics
 		value: {
 			currentValue: {
 				type: Number,
@@ -61,44 +66,191 @@ const ItemSchema = new mongoose.Schema(
 			type: Number,
 			default: 0,
 		},
-		category: {
-			type: String,
-			enum: [
-				"Primary Weapon",
-				"Secondary Weapon",
-				"Melee Weapon",
-				"Consumable Weapon",
-				"Armour",
-				"Garments",
-				"Medical",
-				"Drugs",
-				"Packs",
-				"Tools",
-				"Materials",
-				"Miscellaneous",
-			],
-		},
-		equippable: {
-			type: Boolean,
-			default: false,
-		},
-		baseArmourValue: {
-			type: Number,
-		},
-		damage: {
-			type: Number,
-		},
-		baseChanceToHit: {
-			type: Number,
-			min: [0, "Cannot have a negative base chance to hit."],
-		},
-		consumable: {
-			type: Boolean,
-			default: false,
-		},
-		stackable: {
-			type: Boolean,
-			default: false,
+		properties: {
+			baseProperties: {
+				equippable: {
+					type: Boolean,
+					default: false,
+				},
+				noiseLevel: {
+					type: Number,
+				},
+				condition: {
+					type: Number,
+					min: [0,"Must be a positive number"], // frontned handles rusty etc
+					max: [100, " Must not exceed 100"] // frontend handles qual etc
+				},
+				weight: {
+					type: Number,
+				},
+				baseArmourValue: {
+					type: Number,
+				},
+				damage: {
+					type: Number,
+				},
+				baseChanceToHit: {
+					type: Number,
+					min: [0, "Cannot have a negative base chance to hit."],
+				},
+				consumable: {
+					type: Boolean,
+					default: false,
+				},
+				stackable: {
+					type: Boolean,
+					default: false,
+				},
+			},
+			category: {
+				type: String,
+				enum: [
+					"Primary Weapon",
+					"Secondary Weapon",
+					"Melee Weapon",
+					"Consumable Weapon",
+					"Armour",
+					"Garments",
+					"Medical",
+					"Drugs",
+					"Packs",
+					"Tools",
+					"Materials",
+					"Miscellaneous",
+				],
+				subCategory: {
+					consumableWeapon: {
+						name: {
+							type: String,
+							enum: [
+								"Throwable",
+								"Roonie",
+								"explosive",
+								"Ammunition",
+								"Poison",
+								"Trap",
+								"Distraction",
+							],
+						},
+						properties: {
+							effect: {
+								type: String,
+							},
+							duration: {
+								type: Number,
+							},
+							range: {
+								type: Number,
+							},
+							modifier: {
+								type: Number,
+							},
+							armingTime: {
+								type: Number,
+							},
+							antidoteAvailable: {
+								type: Boolean,
+							},
+						},
+						primaryWeapon: {
+							name: {
+								type: String,
+								enum: ["", "Rifle", "Shotgun"],
+							},
+							properties: {
+								damageType: {
+									type: String,
+									enum: [
+										"", 
+										"", 
+										""],
+								},
+								material: {
+									type: String,
+									enum: [
+										"Iron", 
+										"Steel", 
+										"Wood"],
+								},
+								ammunitionType: {
+									type: String,
+									enum: [
+										"Cartidge", 
+										"Shell", 
+										"Black Powder"],
+								},
+							},
+						},
+						secondaryWeapon: {
+							name: {
+								type: String,
+								enum: [
+									"Handgun", 
+									"Bows", 
+									""],
+							},
+							properties: {
+								damageType: {
+									type: String,
+									enum: ["", "", ""],
+								},
+								material: {
+									type: String,
+									enum: ["Iron", "Steel", "Wood"],
+								},
+								ammunitionType: {
+									type: String,
+									enum: ["Cartidge", "Shell", "Black Powder"],
+								},
+							},
+						},
+						meleeWeapon: {
+							name: {
+								type: String,
+								enum: [
+									"Bladed",
+									"Blunt",
+									"Polearms", // TODO : add more but need a natural balance between the others
+								],
+							},
+							properties: {
+								damageType: {
+									type: String, // TODO: add more but need balancing with others
+									enum: ["", "", ""],
+								},
+								material: {
+									type: String,
+									enum: ["", "", ""],
+								},
+								ammunitionType: {
+									type: String,
+									enum: ["Cartidge", "Shell", "Black Powder"],
+								},
+							},
+						},
+						armour: {
+							name: {
+								type: String,
+								enum: ["", "", ""],
+							},
+							properties: {
+								damageType: {
+									type: String,
+									enum: ["", "", ""],
+								},
+								material: {
+									type: String,
+									enum: ["", "", ""],
+								},
+								ammunitionType: {
+									type: String,
+									enum: ["", "", ""],
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	},
 	{ timestamps: true }

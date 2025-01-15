ce7019e487e6fbd087ed045b5d85e6316fe42e80
Xnrrrrrr <Xnrrrrrr@gmail.com>
2025-01-14 20:26:16 -0500

    added image to StorefrontJS

--- backend/models/Storefront.js
+++ backend/models/Storefront.js
@@ -7,6 +7,9 @@ const StorefrontSchema = new Schema({
         required: true,
         unique: true,
     },
+    image: {
+        type: String,
+    },
     type: {
         type: String,
         enum: [''],

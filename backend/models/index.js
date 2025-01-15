c32c6fc6ca5b0baa2b6ef50870d2a55c8d36fc96
Xnrrrrrr <Xnrrrrrr@gmail.com>
2025-01-15 15:55:15 -0500

    added Npc and WorldJS import/export to index JS

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,15 @@
 const User = require("./User.js");
-const { Crime, FinancialCrime, ViolentCrime, PropertyCrime, ProductionCrime } = require("./Crime.js");
-const Item = require("./Item.js");
-const { LootTable, PenaltyTable } = require("./LootAndPenaltyTable.js");
+const {
+	Crime,
+	FinancialCrime,
+	ViolentCrime,
+	PropertyCrime,
+	ProductionCrime,
+} = require("./Crime.js");
+const Item = require("./Item/Item.js");
+const { LootTable, PenaltyTable } = require("./Item/LootAndPenaltyTable.js");
+const World = require("./World/World.js");
+const Npc = require("./Npc.js");
 
 module.exports = {
 	User,
@@ -13,4 +21,6 @@ module.exports = {
 	Item,
 	LootTable,
 	PenaltyTable,
-}
+	World,
+	Npc,
+};

57bbe69584b4dc51d8c19633f64a4295a6fe60cf
Xnrrrrrr <Xnrrrrrr@gmail.com>
2025-01-17 23:06:16 -0500

    added and implemented wayStationJS

--- backend/models/World/Travel/Waystation.js
+++ backend/models/World/Travel/Waystation.js
@@ -1,5 +1,63 @@
-const mongoose = require("Mongoose");
+const mongoose = require("mongoose");
 
 const WaystationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: {
+            // finish later
+        },
+        coordinates: {
+            type: number,
+            required: true,
+        },
+    },
+    type: {
+        type: String,       /// possibly array
+        enum: [
+            "",
+            "",
+            "",
+            "",
+            "",
 
+
+        ]
+    },
+    Hazards: {                          // needs ref to HazardJS
+        type: [String],
+        enum: [
+            "",
+            "",
+            "",
+            "",
+            "",
+
+        ]
+    },
+    establishedYear: {              // way to establish black market waystations
+        type: Number,
+        required: true,
+    },
+    owner: {
+        type: String,
+        default: " United States Government",
+    },
+    isBlackMarket: {
+        type: boolean,
+    },
+    connectedTravelRoute: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Travel.js"   // ??????? ask streve
+    },
+}, {
+    timestampes: true,
 });
+
+// Model based on Schema
+const Waystation = mongoose.model('Waystation', WaystationSchema);
+
+// Export the model
+module.exports = Waystation;

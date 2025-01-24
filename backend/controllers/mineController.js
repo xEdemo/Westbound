fd009e0412d74f4f5ce43b087876f10284a219ee
Xnrrrrrr <Xnrrrrrr@gmail.com>
2025-01-24 17:46:37 -0500

    pushing changes to mineControllerJS

--- backend/controllers/mineController.js
+++ backend/controllers/mineController.js
@@ -1,4 +1,4 @@
-const asyncHandler = require("express-async-handler");
+ const asyncHandler = require("express-async-handler");
 const { StatusCodes } = require("http-status-codes");
 const { Mine } = require("../models");
 const {
